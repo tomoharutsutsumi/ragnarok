ce790adcdacce4a5f2033fa1ef101e8a8747e0eb
Tsutsumi Tomoharu <ttomoharu201@gmail.com>
2021-08-14 23:15:43 +0900

    draw line

--- app/javascript/bundles/components/Shapes.js
+++ app/javascript/bundles/components/Shapes.js
@@ -3,24 +3,45 @@ import { fabric } from 'fabric'
 
 
 const Shapes = (props) => {
-  const [canvas, setCanvas] = useState('')
+  const [canvas, setCanvas] = useState(null)
+  const [startPoint, setStartPoint] = useState({ top: 0, left: 0 })
 
   useEffect(() => {
     setCanvas(initCanvas());
   }, [])
 
+  useEffect(() => {
+    function handleEvent({ target }) {
+      if(target) {
+        console.log(target.left)
+        console.log(target.top)
+        setStartPoint({ ...startPoint, top: target.top, left: target.left })
+      } else {
+        return;
+      }
+      // setObjectList(canvas.getObjects());
+    }
+    if(canvas) {
+      canvas.on({
+        'selection:created': handleEvent,
+        'selection:updated': handleEvent,
+        'object:moving': handleEvent
+      })
+    }
+  }, [canvas])
+
   const initCanvas = () => (
     new fabric.Canvas('canvas', {
       height: 800,
-      width: 800,
+      width: 1600,
       backgroundColor: '#ffa733'
     })
   )
 
   const addRect = () => {
     const rect = new fabric.Rect({
-      height: 200,
-      width: 280,
+      height: 100,
+      width: 100,
       fill: 'yellow'
     });
     canvas.add(rect);
@@ -33,10 +54,20 @@ const Shapes = (props) => {
     canvas.renderAll();
   }
 
+  const addLine = () => {
+    const { top, left } = startPoint
+    console.log(top)
+    console.log(left)
+    const line = new fabric.Path(`M ${left + 100} ${top + 50} L ${left + 300} ${top + 50}`, { fill: '', stroke: 'black', objectCaching: false });
+    canvas.add(line);
+    canvas.renderAll();
+  }
+
   return (
     <div>
-      <button onClick={() => addText(canvas)}>Text</button>
-      <button onClick={() => addRect(canvas)}>Rectangle</button>
+      <button onClick={() => addText()}>Text</button>
+      <button onClick={() => addRect()}>Rectangle</button>
+      <button onClick={() => addLine()}>Line</button>
       <canvas id='canvas' />
     </div>
   );
